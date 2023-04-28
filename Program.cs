aa384e2e06d30c15eb477a50af36e351e015f32d
Johan Valentino Ghinelli <116793076+jj-sium@users.noreply.github.com>
2023-04-28 09:11:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+Console.WriteLine("Hello, DBWorld!");
+// Connessione al db
+SQLiteConnection cn1 = new SQLiteConnection("chinook.db");
+var tblArtists = cn1.Query<Artist>("select * from artists");
+Console.WriteLine($"In questa tabella ci sono {tblArtists.Count} record!");
+
+//-------------
+public class Artist
+{
+    public int ArtistId { get; set; }
+    public string Name { get; set; }
+}
